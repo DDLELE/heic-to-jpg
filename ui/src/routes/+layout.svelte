5fcbac674b324f5bf53cc983f3b8b3a7dc3e3769
DanielRailean <daniel.railean1@gmail.com>
2025-06-03 21:41:09 +0200

    state load

---
+++ ui/src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { staticConfig } from '$lib/config';
+	import './styles.css';
+	import Toasts from '$lib/components/Toasts.svelte';
+</script>
+
+<svelte:head>
+	<title>{staticConfig.name}</title>
+</svelte:head>
+
+<Toasts />
+<div class="flex flex-row min-h-[100vh]">
+
+	<main class="w-full h-full">
+		<div
+			class="flex flex-col m-8 shadow shadow-stone-300 dark:shadow-zinc-800 bg-white rounded-xl dark:bg-[#1E2021] dark:text-stone-50"
+		>
+			<slot />
+		</div>
+	</main>
+</div>
+
+<style>
+</style>
---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { addToast } from '$lib/toastStore';
+	import TreeWrapper from '../lib/components/treeWrapper.svelte';
+
+	import { apiService } from '$lib/requests';
+	import { onMount } from 'svelte';
+	import Spinner from '$lib/components/Spinner.svelte';
+
+	let info: any | undefined;
+	onMount(async () => {
+
+	});
+</script>
+
+<div>
+	<h1 class="text-xl text-center p-2">
+		<div class="flex flex-row items-center justify-center">
+
+	Hi Mom</h1>
+
+
+</div>
